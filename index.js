2ad98bdf328d6451ada0fa67a5770adb1a18c51c
camillysilva21 <00001096357884sp@al.educacao.sp.gov.br>
2024-10-17 13:59:28 -0300

    alteracoes

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const avanca = document.querySelectorAll(".btn-proximo");
 
 avanca.forEach(button =>){
-     button.addEventllister("click", funccc
+     button.addEventllister("click", function(){
+        const atual = document.querySelector(".ativo");
+        const proximopasso =
+
+ 
+     }
 
 
 
